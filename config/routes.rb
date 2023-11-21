8437c78b23bab9eccacc89ac11d076421f37eb50
Simone Roveri <spikeroveri91@gmail.com>
2023-11-21 20:23:13 +0900

    fixed misspell in controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'profile#index'
-  resources :profile
+  root 'profiles#index'
+  resources :profiles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
