4d9516a319a2e0745a8496d18aaea3d1330ad026
Simone Roveri <spikeroveri91@gmail.com>
2023-12-28 14:34:07 +0900

    add it to the model rspec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a first name, surname, bithday"
+  it "is invalid without a first name"
+  it "is invalid without a surname"
+  it "is invalid without a birthday"
+  it "is invalid without a linkedln"
+  it "is invalid with a duplicate linkedln"
+  it "returns a user's full name as a astring "
 end
