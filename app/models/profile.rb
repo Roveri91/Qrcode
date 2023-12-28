2dae04ad25e236f5b86c77d717a0c9fb24458173
Simone Roveri <spikeroveri91@gmail.com>
2023-12-28 15:37:29 +0900

    updated validation for linkedln file

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,6 +8,9 @@ class Profile < ApplicationRecord
   validates :birthday, presence: true
   validates :linkedln, presence: true
   validates :linkedln, uniqueness: { message: "This URL has already taken." }
+
+  validates :linkedln, presence: true, format: { with: %r{\Ahttps://www\.linkedin\.com/in/[a-zA-Z0-9-]+(?:/)?\z}, message: 'is not a valid LinkedIn profile link' }
+
   # validates :linkedln, presence: true, format: { with: /\Ahttps:\/\/www.linkedin.com\/in\/[a-zA-Z0-9_-]+\z/, message: "ust be a valid LinkedIn URL" }
 
   # before_commit :generate_qrcode, on: :create
