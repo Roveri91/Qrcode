22cac7f1ccb7152b7e9826b3ae06a9d54113ae6f
Simone Roveri <spikeroveri91@gmail.com>
2023-12-31 12:08:45 +0900

    add validation for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :profile
+
+  validates :title, presence: true, uniqueness: { scope: :profile_id }
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :profile, presence: true
 end
