1e486c2a26e4cc8203d214479a380b5827072b13
Simone Roveri <spikeroveri91@gmail.com>
2023-12-31 12:00:00 +0900

    seed Article

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 p "Deleting all previous record"
 
+Article.delete_all
 Profile.delete_all
 
 p "Delete them"
@@ -15,5 +16,33 @@ p "Creating few profile"
 
 profile = Profile.create!(name: "Simone", surname: "Roveri", linkedln: "https://www.linkedin.com/in/simone-roveri/", birthday: "1991-06-14")
 p "Create #{profile.name} profile"
+
+p "Done!!"
+
+p "Creating few article"
+
+article = Article.create!(
+  profile: profile,
+  title: "10 Tips for Successful Ruby on Rails Development",
+  content: <<-CONTENT.strip_heredoc
+    Ruby on Rails is a powerful web development framework that simplifies the development process
+    and encourages best practices. Here are 10 tips to enhance your Ruby on Rails development:
+
+    1. **Follow RESTful conventions:** Design your routes, controllers, and models following RESTful principles.
+    2. **Use partials and helpers:** Keep your views DRY by using partials and helpers for reusable code.
+    3. **Optimize database queries:** Use ActiveRecord queries efficiently to minimize database hits.
+    4. **Implement background jobs:** Offload time-consuming tasks to background jobs using tools like Sidekiq.
+    5. **Write tests:** Embrace Test-Driven Development (TDD) and write tests for your models, controllers, and views.
+    6. **Secure your application:** Implement proper authentication and authorization mechanisms to secure your app.
+    7. **Optimize asset delivery:** Use tools like Webpacker to manage and optimize your front-end assets.
+    8. **Monitor performance:** Use tools like New Relic or Scout to monitor and optimize your application's performance.
+    9. **Apply caching:** Implement caching strategies to improve response times.
+    10. **Stay up-to-date:** Keep your Ruby on Rails version and gems up-to-date for security and performance improvements.
+
+    These tips will help you build robust, scalable, and maintainable Ruby on Rails applications.
+    CONTENT
+)
+
+p "Create #{article.title} !"
 
 p "Done!!"
