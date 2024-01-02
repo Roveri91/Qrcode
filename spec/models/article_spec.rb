f82078cf6c6ea42290456678188bd8bb337c4dcb
Simone Roveri <spikeroveri91@gmail.com>
2024-01-02 10:38:19 +0900

    create a test for article CHECK duplicate article name for same profile

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "does not allow duplicate article names per profile" do
+    profile = Profile.create(
+      name: "Bill",
+      surname: "Gates",
+      birthday: "1955-10-28",
+      linkedln: "https://www.linkedin.com/in/williamhgates/"
+    )
+
+    profile.articles.create(
+      title: "Testing on rails",
+      content: "Just buy Evereday Rails book"
+    )
+
+    new_article = profile.articles.build(
+      title: "Testing on rails",
+      content: "lorem aksfnie fnewfoiwnvdsj bfewo"
+    )
+
+    new_article.valid?
+    expect(new_article.errors[:title]).to include("has already been taken")
+  end
+
+
 end
