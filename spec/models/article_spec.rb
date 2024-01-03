48b05808d698c0c59ea7f022cea062454fb3db39
Simone Roveri <spikeroveri91@gmail.com>
2024-01-03 17:16:05 +0900

    test that check if it is allowed for two profiles to have the same article title

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -16,12 +16,39 @@ RSpec.describe Article, type: :model do
 
     new_article = profile.articles.build(
       title: "Testing on rails",
-      content: "lorem aksfnie fnewfoiwnvdsj bfewo"
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Mauris pharetra et ultrices neque ornare aenean euismod. Arcu bibendum at varius vel pharetra vel turpis nunc. "
     )
 
     new_article.valid?
     expect(new_article.errors[:title]).to include("has already been taken")
   end
 
+  it "allows two profile to share a article name" do
+    profile = Profile.create(
+      name: "Bill",
+      surname: "Gates",
+      birthday: "1955-10-28",
+      linkedln: "https://www.linkedin.com/in/williamhgates/"
+    )
+
+    profile.articles.create(
+      title: "Testing on rails",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Mauris pharetra et ultrices neque ornare aenean euismod. Arcu bibendum at varius vel pharetra vel turpis nunc. "
+    )
+
+    other_profile = Profile.create(
+      name: "Tobias",
+      surname: "Lütke",
+      birthday: "1981-07-16",
+      linkedln: "https://www.linkedin.com/in/tobiaslutke/"
+    )
+
+    other_article = other_profile.articles.build(
+      title: "Testing on rails",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Mauris pharetra et ultrices neque ornare aenean euismod. Arcu bibendum at varius vel pharetra vel turpis nunc. "
+    )
+
+    expect(other_article).to be_valid
+  end
 
 end
