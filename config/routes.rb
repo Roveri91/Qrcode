a6cf403b4df2f194a5c8c550bee7ff9760d15751
Simone Roveri <spikeroveri91@gmail.com>
2024-01-08 12:05:55 +0900

    when click on new article finally something is shown

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'article/new'
+  get 'articles/new'
   root 'profiles#index'
   resources :profiles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
