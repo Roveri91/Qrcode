43ace86d832e4d1dffcb1d79508be54b688fed55
Simone Roveri <spikeroveri91@gmail.com>
2024-01-11 19:22:26 +0900

    can create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,34 @@
 class ArticlesController < ApplicationController
+  before_action :set_profile
+
   def new
-    @article = Article.new
+    @article = @profile.articles.build
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = @profile.articles.build(article_params)
     @article.save
+
     # if @profile.save
     #   redirect_to profile_path(@profile)
     # else
     #   render :new
     # end
 
-    respond_to do |format|
-      if @article.save
-        format.turbo_stream { render :create, locals: { article: @article } }
-      else
-        format.turbo_stream { render :new, status: :unprocessable_entity, locals: { article: @article } }
-      end
-    end
+    # respond_to do |format|
+    #   if @article.save
+    #     format.turbo_stream { render :create, locals: { article: @article } }
+    #   else
+    #     format.turbo_stream { render :new, status: :unprocessable_entity, locals: { article: @article } }
+    #   end
+    # end
   end
 
   private
+
+  def set_profile
+    @profile = Profile.find(params[:profile_id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content, :profile)
