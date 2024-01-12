26fe1a2760ff1ff9b21ddcbd31c42ee901125c85
Simone Roveri <spikeroveri91@gmail.com>
2024-01-12 19:16:14 +0900

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get 'comments/new'
   root 'profiles#index'
-  resources :profiles do
-    resources :articles, only: [:new, :create] do
-      resources :comments, only: [:new, :create]
-    end
-  end
+  resources :profiles
+  resources :articles, only: [:new, :create, :show]
+  resources :comments, only: [:new, :create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
