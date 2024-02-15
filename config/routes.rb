034ba7384e59ca127bb9858ded5e7fbd9b2bda93
Simone Roveri <spikeroveri91@gmail.com>
2024-02-15 11:03:28 +0900

    move comments routes in articles resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # get 'comments/new'
   resources :profiles, only: [:index, :show, :new, :create] do
-    resources :articles, only: [:new, :create, :show]
+    resources :articles, only: [:new, :create, :show] do
+      resources :comments, only: [:new, :create]
+    end
   end
-  resources :comments, only: [:new, :create]
 
   root 'profiles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
