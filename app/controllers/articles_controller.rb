a2e2f490e6e0782b2c637135629e3cc74a0b3626
Simone Roveri <spikeroveri91@gmail.com>
2024-02-21 19:14:32 +0900

    create before action 'set_article' for show and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_profile, only: %i[new create show destroy]
+  before_action :set_article, only: %i[show destroy]
 
   def new
     # @profile = Profile.find(params[:profile_id])
@@ -17,24 +18,13 @@ class ArticlesController < ApplicationController
       render :new
     end
 
-    # respond_to do |format|
-    #   if @article.save
-    #     format.turbo_stream { render :create, locals: { article: @article } }
-    #   else
-    #     format.turbo_stream { render :new, status: :unprocessable_entity, locals: { article: @article } }
-    #   end
-    # end
   end
 
   def show
-    # @profile = Profile.find(params[:id])
-    # @article = @profile.article.find(params[:id])
-    @article = Article.find(params[:id])
 
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to profile_path(@profile), status: :see_other
@@ -48,6 +38,10 @@ class ArticlesController < ApplicationController
 
   def set_profile
     @profile = Profile.find(params[:profile_id])
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
