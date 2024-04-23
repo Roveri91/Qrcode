d78fc6af800e3a76d1c36de10e3a45ce91a3f806
Simone Roveri <spikeroveri91@gmail.com>
2024-04-23 20:04:15 +0900

    test comment.search function with no matches

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Comment, type: :model do
 
     article = profile.articles.create(
       title: "Testing on rails",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Mauris pharetra et ultrices neque ornare aenean euismod. Arcu bibendum at varius vel pharetra vel turpis nunc. "
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     )
 
     comment1 = article.comments.create(
@@ -29,6 +29,35 @@ RSpec.describe Comment, type: :model do
 
     expect(Comment.search("first")).to include(comment1, comment3)
     expect(Comment.search("first")).to_not include(comment2)
+  end
+
+  it "returns an empty collection when no results are found" do
+    profile = Profile.create(
+      name: "Bill",
+      surname: "Gates",
+      birthday: "1955-10-28",
+      linkedln: "https://www.linkedin.com/in/williamhgates/"
+    )
+
+    article = profile.articles.create(
+      title: "Testing on rails",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    )
+
+    comment1 = article.comments.create(
+      content: "This is the first comment",
+      profile: profile
+    )
+    comment2 = article.comments.create(
+      content: "this is the second comment",
+      profile: profile
+    )
+    comment3 = article.comments.create(
+      content: "this is not the first comment",
+      profile: profile
+    )
+
+    expect(Comment.search("message")).to be_empty
   end
 
 end
