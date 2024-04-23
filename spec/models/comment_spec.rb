4668c94ae57405761e3c179094c15f01cd3258dd
Simone Roveri <spikeroveri91@gmail.com>
2024-04-23 21:12:16 +0900

    refactured comment.search test using before do to declare variable only once

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,63 +1,108 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it "returns comments that match the search term" do
-    profile = Profile.create(
+  before do
+    @profile = Profile.create(
       name: "Bill",
       surname: "Gates",
       birthday: "1955-10-28",
       linkedln: "https://www.linkedin.com/in/williamhgates/"
     )
 
-    article = profile.articles.create(
+    @article = @profile.articles.create(
       title: "Testing on rails",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     )
-
-    comment1 = article.comments.create(
-      content: "this is the first comment",
-      profile: profile
-    )
-    comment2 = article.comments.create(
-      content: "this is the second comment",
-      profile: profile
-    )
-    comment3 = article.comments.create(
-      content: "this is not the first comment",
-      profile: profile
-    )
-
-    expect(Comment.search("first")).to include(comment1, comment3)
-    expect(Comment.search("first")).to_not include(comment2)
   end
 
-  it "returns an empty collection when no results are found" do
-    profile = Profile.create(
-      name: "Bill",
-      surname: "Gates",
-      birthday: "1955-10-28",
-      linkedln: "https://www.linkedin.com/in/williamhgates/"
-    )
+  describe "search message for a term" do
+    before do
+      @comment1 = @article.comments.create(
+        content: "this is the first comment",
+        profile: @profile
+      )
+      @comment2 = @article.comments.create(
+        content: "this is the second comment",
+        profile: @profile
+      )
+      @comment3 = @article.comments.create(
+        content: "this is not the first comment",
+        profile: @profile
+      )
+    end
 
-    article = profile.articles.create(
-      title: "Testing on rails",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    )
+    context "when a match is found" do
+      # matching exaple ...
+      it "returns comments that match the search term" do
+        expect(Comment.search("first")).to include(@comment1, @comment3)
+        expect(Comment.search("first")).to_not include(@comment2)
+      end
+    end
 
-    comment1 = article.comments.create(
-      content: "This is the first comment",
-      profile: profile
-    )
-    comment2 = article.comments.create(
-      content: "this is the second comment",
-      profile: profile
-    )
-    comment3 = article.comments.create(
-      content: "this is not the first comment",
-      profile: profile
-    )
-
-    expect(Comment.search("message")).to be_empty
+    context "when no match is found " do
+      # non-matching examples ...
+      it "returns an empty collection when no results are found" do
+        expect(Comment.search("message")).to be_empty
+      end
+    end
   end
+  # it "returns comments that match the search term" do
+  #   profile = Profile.create(
+  #     name: "Bill",
+  #     surname: "Gates",
+  #     birthday: "1955-10-28",
+  #     linkedln: "https://www.linkedin.com/in/williamhgates/"
+  #   )
+
+  #   article = profile.articles.create(
+  #     title: "Testing on rails",
+  #     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  #   )
+
+  #   comment1 = article.comments.create(
+  #     content: "this is the first comment",
+  #     profile: profile
+  #   )
+  #   comment2 = article.comments.create(
+  #     content: "this is the second comment",
+  #     profile: profile
+  #   )
+  #   comment3 = article.comments.create(
+  #     content: "this is not the first comment",
+  #     profile: profile
+  #   )
+
+  #   expect(Comment.search("first")).to include(comment1, comment3)
+  #   expect(Comment.search("first")).to_not include(comment2)
+  # end
+
+  # it "returns an empty collection when no results are found" do
+  #   profile = Profile.create(
+  #     name: "Bill",
+  #     surname: "Gates",
+  #     birthday: "1955-10-28",
+  #     linkedln: "https://www.linkedin.com/in/williamhgates/"
+  #   )
+
+  #   article = profile.articles.create(
+  #     title: "Testing on rails",
+  #     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  #   )
+
+  #   comment1 = article.comments.create(
+  #     content: "This is the first comment",
+  #     profile: profile
+  #   )
+  #   comment2 = article.comments.create(
+  #     content: "this is the second comment",
+  #     profile: profile
+  #   )
+  #   comment3 = article.comments.create(
+  #     content: "this is not the first comment",
+  #     profile: profile
+  #   )
+
+  #   expect(Comment.search("message")).to be_empty
+  # end
 
 end
