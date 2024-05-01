7925111c1adc6cf2cb1038b48ecc9eb0828c6bf1
Simone Roveri <spikeroveri91@gmail.com>
2024-05-01 18:27:50 +0900

    add validates to comment: content presence and length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :profile
   belongs_to :article
 
+  validates :content, presence: true;
+  validates :content, length: { in: 10..100 }
+
   scope :search, ->(term) {
     where("LOWER(content) LIKE ?", "%#{term.downcase}%")
   }
