6cea7270d5134d1e61ba324b796d46ff03ab18c8
Simone Roveri <spikeroveri91@gmail.com>
2024-05-01 20:52:29 +0900

    add comment rspec to check the correct working of the validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,11 +13,8 @@ RSpec.describe Comment, type: :model do
       title: "Testing on rails",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     )
-  end
 
-  describe "search message for a term" do
-    before do
-      @comment1 = @article.comments.create(
+    @comment1 = @article.comments.create(
         content: "this is the first comment",
         profile: @profile
       )
@@ -29,7 +26,46 @@ RSpec.describe Comment, type: :model do
         content: "this is not the first comment",
         profile: @profile
       )
-    end
+  end
+
+  it "is valid with a content of 10 char" do
+    comment = @article.comments.create(
+      content: "HELLO SIMO",
+      profile: @profile
+    )
+    expect(comment).to be_valid
+  end
+
+  it "is valid with a content of 100 char" do
+    comment = @article.comments.create(
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere quam nec quam ullamcorper!!",
+      profile: @profile
+    )
+    expect(comment).to be_valid
+  end
+
+  it "is invalid with a content shorter than 10 char (9)" do
+    comment = @article.comments.create(
+      content: "# Hello!!",
+      profile: @profile
+    )
+    comment.valid?
+    expect(comment.errors[:content]).to include("is too short (minimum is 10 characters)")
+  end
+
+  it "is invalid with a content longer than 100 char (101)" do
+    comment = @article.comments.create(
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere quam nec quam ullamcorper!!!",
+      profile: @profile
+    )
+    comment.valid?
+    expect(comment.errors[:content]).to include("is too long (maximum is 100 characters)")
+  end
+
+  describe "search message for a term" do
+    # before do
+
+    # end
 
     context "when a match is found" do
       # matching exaple ...
