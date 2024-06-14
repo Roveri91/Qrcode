4afbe664ca6decbb374af2ecb00a66073a720569
Roveri91 <spikeroveri91@gmail.com>
2024-06-14 15:52:11 +0900

    tested factory_bot

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
+  it "has a valid factory" do
+    expect(FactoryBot.build(:profile)).to be_valid
+  end
+
   it "is valid with a first name, surname, bithday, linkedln" do
     profile = Profile.new(
       name: "Bill",
