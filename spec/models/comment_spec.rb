7328cebdae5a9e0c699c5bc03f2d3e84452aca7a
Roveri91 <spikeroveri91@gmail.com>
2024-06-14 13:19:58 +0900

    added some describe to test for improve readability

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -28,40 +28,41 @@ RSpec.describe Comment, type: :model do
     )
   end
 
-  it "is valid with a content of 10 char" do
-    comment = @article.comments.create(
-      content: "HELLO SIMO",
-      profile: @profile
-    )
-    expect(comment).to be_valid
-  end
+  describe "validations" do
+    it "is valid with a content of 10 char" do
+      comment = @article.comments.create(
+        content: "HELLO SIMO",
+        profile: @profile
+      )
+      expect(comment).to be_valid
+    end
 
-  it "is valid with a content of 100 char" do
-    comment = @article.comments.create(
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere quam nec quam ullamcorper!!",
-      profile: @profile
-    )
-    expect(comment).to be_valid
-  end
+    it "is valid with a content of 100 char" do
+      comment = @article.comments.create(
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere quam nec quam ullamcorper!!",
+        profile: @profile
+      )
+      expect(comment).to be_valid
+    end
 
-  it "is invalid with a content shorter than 10 char (9)" do
-    comment = @article.comments.create(
-      content: "# Hello!!",
-      profile: @profile
-    )
-    comment.valid?
-    expect(comment.errors[:content]).to include("is too short (minimum is 10 characters)")
-  end
+    it "is invalid with a content shorter than 10 char (9)" do
+      comment = @article.comments.create(
+        content: "# Hello!!",
+        profile: @profile
+      )
+      comment.valid?
+      expect(comment.errors[:content]).to include("is too short (minimum is 10 characters)")
+    end
 
-  it "is invalid with a content longer than 100 char (101)" do
-    comment = @article.comments.create(
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere quam nec quam ullamcorper!!!",
-      profile: @profile
-    )
-    comment.valid?
-    expect(comment.errors[:content]).to include("is too long (maximum is 100 characters)")
+    it "is invalid with a content longer than 100 char (101)" do
+      comment = @article.comments.create(
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere quam nec quam ullamcorper!!!",
+        profile: @profile
+      )
+      comment.valid?
+      expect(comment.errors[:content]).to include("is too long (maximum is 100 characters)")
+    end
   end
-
   describe "search message for a term" do
     context "when a match is found" do
       # matching examples ...
